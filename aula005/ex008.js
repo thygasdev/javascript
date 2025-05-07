480a4738b3a2fa5a3f1d82079598ad94f6f5bee3
thygasdev <espadathygas@gmail.com>
2025-05-07 15:33:48 -0300

    ar

---
+++ aula005/ex008.js
@@ -0,0 +1,4 @@
+var vec = 14544
+if (vec>= 60) {
+    console.log(`multado passou com ${vec} km/h`)
+}
---
+++ aula005/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'eua'
+console.log(`vivendo em ${pais}`)
+if ( pais == 'brasil') {
+    console.log(' guerreiro br')
+} else {
+    console.log('gringo safado! ATIRAAAAAAAA')
+
+}
